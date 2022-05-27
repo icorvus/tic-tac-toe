7b49e6f9746681526f035fa97e750588d374716e
icorvus <business.wrona@gmail.com>
2022-05-27 22:22:43 +0200

    Added basic project files

---
+++ player.py
@@ -0,0 +1,8 @@
+
+class Player:
+    def __init__(self, name: str, sign: chr):
+        assert len(name) < 30, f"The player name: {name} is too long!(>30 characters)"
+        assert sign in ["X", "O", "x", "o"], f"The sign is not an \"X\" or not an \"O\""
+
+        self.name = name
+        self.sign = sign

94e129a0b80e543b496392ffd02220eaaeed6936
icorvus <business.wrona@gmail.com>
2022-05-30 13:06:36 +0200

    Created method to check if game is over

--- board.py
+++ board.py
@@ -1,9 +1,19 @@
 class Board:
+    WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     def __init__(self):
-        self.state = [None for x in range(9)]
+        self.state = ['_' for x in range(9)]
     
     def print_board(self):
-        pass
+        return """
+ {} | {} | {}
+———————————
+ {} | {} | {}
+———————————
+ {} | {} | {}
+        """.format(*self.state)
 
-    def check_result(self):
-        pass
+    def isGameOver(self, sign):
+        for combo in self.WINNING_COMBOS:
+            if self.state[combo[0]] == self.state[combo[1]] == self.state[combo[2]] == sign:
+                return True
+        return False

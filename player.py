42b859cea95bab31d68801a8626542089af583f8
icorvus <business.wrona@gmail.com>
2022-05-31 19:11:27 +0200

    Minor fix

--- player.py
+++ player.py
@@ -1,12 +1,7 @@
-from board import Board
-
 class Player:
-    def __init__(self, name: str, sign: chr):
+    def __init__(self, name: str, sign: str):
         assert len(name) < 30, f"The player name: {name} is too long!(>30 characters)"
         assert sign in ["X", "O", "x", "o"], f"The sign is not an \"X\" or not an \"O\""
 
         self.name = name
-        self.sign = sign
-
-    def make_a_move(self, move, board):
-        board.state[move - 1] = "X"
+        self.sign = sign.lower()

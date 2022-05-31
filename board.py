eb73d96e3c355d1e074103346ce492482b379c8d
icorvus <business.wrona@gmail.com>
2022-05-31 17:31:08 +0200

    Added __repr__ and method to safely alter board state

--- board.py
+++ board.py
@@ -1,7 +1,11 @@
 class Board:
     WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    def __init__(self):
-        self.state = ['_' for x in range(9)]
+    def __init__(self, state=None):
+        if state:
+            self.__state = state
+        else:
+            self.__state = ['_' for x in range(9)]
+        
     
     def __str__(self):
         return """
@@ -10,17 +14,24 @@ class Board:
  {} | {} | {}
 ———————————
  {} | {} | {}
-        """.format(*self.state)
+        """.format(*self.__state)
+
+    def __repr__(self):
+        return f"Board({self.__state})"
+
+    def update(self, move: int, sign):
+        if not self.is_move_legal(move):
+            return False
+        self.__state[move - 1] = sign
+        return True
 
     def is_game_over(self, sign):
         for combo in self.WINNING_COMBOS:
-            if self.state[combo[0]] == self.state[combo[1]] == self.state[combo[2]] == sign:
+            if self.__state[combo[0]] == self.__state[combo[1]] == self.__state[combo[2]] == sign:
                 return True
         return False
 
-
-
-    def is_move_legal(self, move):
-        if move in range(1, 10) and self.state[move - 1] == '_':
+    def is_move_legal(self, move: int):
+        if move in range(1, 10) and self.__state[move - 1] == '_':
             return True
         return False
